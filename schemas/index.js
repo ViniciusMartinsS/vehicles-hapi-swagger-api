d5e75fdb6a3c1cc7c65665ff08fa06b3b2930141
Vinicius Martins <visimonemartins@hotmail.com>
2019-12-21 10:39:58 -0300

    Add index exportation on schemas

---
+++ schemas/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const {
+  SelectVehicle,
+  CreateVehicle,
+  UpdateVehicle,
+  DeleteVehicle
+} = require('./vehicle')
+
+module.exports = {
+  SelectVehicle,
+  CreateVehicle,
+  UpdateVehicle,
+  DeleteVehicle
+}
