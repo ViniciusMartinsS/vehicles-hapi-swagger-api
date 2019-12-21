f4b45c0502d17e13d954faa8259109ce6b43dc93
Vinicius Martins <visimonemartins@hotmail.com>
2019-12-21 10:37:36 -0300

    Add schema validation folder

---
+++ schemas/vehicle.js
@@ -0,0 +1,76 @@
+'use strict'
+
+module.exports.SelectVehicle = {
+  params: {
+    id: Joi.number()
+      .label('id')
+      .optional()
+      .description('The id of a vehicle'),
+  }
+}
+
+module.exports.CreateVehicle = {
+  payload: {
+    brand: Joi.string()
+      .trim()
+      .replace(/\s{2,}/g, ' ')
+      .label('Brand')
+      .required()
+      .description('The brand of a vehicle'),
+
+    model: Joi.string()
+      .trim()
+      .replace(/\s{2,}/g, ' ')
+      .label('Model')
+      .required()
+      .description('The model of a vehicle'),
+
+    version: Joi.string()
+      .trim()
+      .replace(/\s{2,}/g, ' ')
+      .label('Version')
+      .required()
+      .description('The version of a vehicle')
+  }
+}
+
+module.exports.UpdateVehicle = {
+  params: {
+    id: Joi.number()
+      .label('id')
+      .required()
+      .description('The id of a vehicle'),
+  },
+
+  payload: {
+    brand: Joi.string()
+      .trim()
+      .replace(/\s{2,}/g, ' ')
+      .label('Brand')
+      .optional()
+      .description('The brand of a vehicle'),
+
+    model: Joi.string()
+      .trim()
+      .replace(/\s{2,}/g, ' ')
+      .label('Model')
+      .optional()
+      .description('The model of a vehicle'),
+
+    version: Joi.string()
+      .trim()
+      .replace(/\s{2,}/g, ' ')
+      .label('Version')
+      .optional()
+      .description('The version of a vehicle')
+  }
+}
+
+module.exports.DeleteVehicle = {
+  params: {
+    id: Joi.number()
+      .label('id')
+      .require()
+      .description('The id of a vehicle'),
+  }
+}
